1b2a357e723720cc9efbc0c9df90a163c941e06c
rwenh <linuxmintoem@gmail.com>
2026-01-31 21:49:11 +0530

    Rust module upgrade

--- nvim/lua/plugins/specs/lang/rust.lua
+++ nvim/lua/plugins/specs/lang/rust.lua
@@ -5,40 +5,37 @@ return {
     version = "^5",
     ft = "rust",
     config = function()
-    vim.g.rustaceanvim = {
-      server = {
-        on_attach = function(_, bufnr)
-        vim.keymap.set("n", "<leader>ra", function()
-        vim.cmd.RustLsp("codeAction")
-        end, { buffer = bufnr, desc = "Rust Code Action" })
-
-        vim.keymap.set("n", "<leader>rd", function()
-        vim.cmd.RustLsp("debuggables")
-        end, { buffer = bufnr, desc = "Rust Debuggables" })
-
-        vim.keymap.set("n", "<leader>rr", function()
-        vim.cmd.RustLsp("runnables")
-        end, { buffer = bufnr, desc = "Rust Runnables" })
-        end,
-      },
-    }
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(_, bufnr)
+            vim.keymap.set("n", "<leader>ra", function() 
+              vim.cmd.RustLsp("codeAction") 
+            end, { buffer = bufnr, desc = "Rust Code Action" })
+            
+            vim.keymap.set("n", "<leader>rd", function() 
+              vim.cmd.RustLsp("debuggables") 
+            end, { buffer = bufnr, desc = "Rust Debuggables" })
+            
+            vim.keymap.set("n", "<leader>rr", function() 
+              vim.cmd.RustLsp("runnables") 
+            end, { buffer = bufnr, desc = "Rust Runnables" })
+          end,
+        },
+      }
     end,
   },
   {
     "saecki/crates.nvim",
     event = "BufRead Cargo.toml",
     config = function()
-    require("crates").setup({
-      completion = {
-        cmp = { enabled = true }
-      },
-      lsp = {
-        enabled = true,
-        actions = true,
-        completion = true,
-        hover = true,
-      },
-    })
+      require("crates").setup({
+        lsp = {
+          enabled = true,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      })
     end,
   },
 }
