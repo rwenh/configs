6d89b4012c7824271d3c96b45713b0920c442d6a
rwenh <linuxmintoem@gmail.com>
2026-04-03 10:31:40 +0530

    sqlfmt formatter for SQL and MySQL via conform optional extension

--- nvim/lua/plugins/specs/lang/sql.lua
+++ nvim/lua/plugins/specs/lang/sql.lua
@@ -1,17 +1,14 @@
--- lua/plugins/specs/lang/sql.lua
--- dadbod + UI are fully managed in database.lua.
--- This file adds SQL-specific conform formatter and lint only.
+-- lua/plugins/specs/lang/sql.lua - SQL development
 
 return {
   -- Conform: sqlfmt formatter
   {
     "stevearc/conform.nvim",
     optional = true,
-    opts = {
-      formatters_by_ft = {
-        sql   = { "sqlfmt" },
-        mysql = { "sqlfmt" },
-      },
-    },
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.sql   = { "sqlfmt" }
+      opts.formatters_by_ft.mysql = { "sqlfmt" }
+    end,
   },
 }
